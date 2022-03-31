c5c7493e97ba2b8c2586d1ef0fe53aa45c943715
Alexandre Do Pham <alexandre34980@outlook.fr>
2022-03-31 11:45:14 -0400

    Add routes

--- api-front/src/Utils/Api.ts
+++ api-front/src/Utils/Api.ts
@@ -2,4 +2,9 @@ export const baseURL = "http://localhost:8080/";
 
 export const API = {  
     post : baseURL + "api/post",
+    postAccount :  baseURL + "api/post/newAccount", // Création d'un compte
+    getAccounts : baseURL + "api/get/allAccounts", // Récupération des comptes existants
+    deleteAccount: baseURL + "api/delete", // Suppression d'un compte
+    putAccount: baseURL + "api/put/changeAccount", // Modification d'un compte
+    postLogin: baseURL + "api/post/login", // Connexion d'un compte
 }
